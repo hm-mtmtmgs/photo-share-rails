9190573742124af7556493eb6c926113014196a5
Matsumoto Hiromi <hiromi.mtmtmgs@gmail.com>
2020-03-14 16:50:55 +0900

    usersコントローラーとpostsコントローラーとhomeコントローラーを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def add
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/add'
+  get 'posts/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/add'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
