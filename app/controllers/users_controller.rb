dcfa633cba3cbec7e92e06510eca8640c91ea7fd
Matsumoto Hiromi <hiromi.mtmtmgs@gmail.com>
2020-03-17 02:09:25 +0900

    新規登録画面のパスワード一致確認機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :password, :icon, :email, :birthday)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-	validates :name, { presence: { message: "：を入力してください。" },
-                     length: { maximum: 20, message: "：20文字以内で入力してください。" } }
+	validates :name, { presence: { message: "を入力してください。" },
+                     length: { maximum: 20, message: "は20文字以内で入力してください。" } }
 
-	validates :email, { presence: { message: " ：を入力してください。" },
-                      uniqueness: { message: "：は既に使用されています。" } }
+	validates :email, { presence: { message: "を入力してください。" },
+                      uniqueness: { message: "は既に使用されています。" } }
 
-	validates :password, { presence: { message: "：を入力してください。" },
-                         length: { minimum: 6, message: "：6文字以上入力してください。" },
-                         length: { maximum: 12, message: "：12文字以内で入力してください。" },
-                         format: { with: /[0-9a-zA-Z]/, message: "：半角英数字で入力してください。" } }
+	validates :password, { presence: { message: "を入力してください。" },
+                         length: { within: 6..12, message: "は6文字以上12文字以内で入力してください。" },
+                         format: { with: /[0-9a-zA-Z]/, message: "は半角英数字で入力してください。" },
+											   confirmation: { message: "パスワードが一致しません。"} }
 end
