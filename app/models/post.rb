e640e997bb1942a4706c5c690990430b963209e1
Matsumoto Hiromi <hiromi.mtmtmgs@gmail.com>
2020-03-22 21:09:09 +0900

    新規投稿のバリデーションを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
 	mount_uploader(:image, ImageUploader)
+
+	validates :image, { presence: { message: "を選択してください" } }
+
+	validates :title, { presence: { message: "を入力してください" },
+											length: { maximum: 30, message: "は30文字以内で入力してください" } }
+
+	validates :message, { presence: { message: "を入力してください" },
+												length: { maximum: 200, message: "は200文字以内で入力してください" } }
 end
