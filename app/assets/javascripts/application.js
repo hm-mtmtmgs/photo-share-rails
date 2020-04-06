ee41244f7809e3a3de44c157f97ffc204f4f0c5e
Matsumoto Hiromi <hiromi.mtmtmgs@gmail.com>
2020-04-06 18:50:47 +0900

    トップページへのスクロールボタン機能を実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,21 @@ $(document).ready(function() {
     window.setTimeout("$('#alert').fadeOut()", 2500);
   });
 });
+
+$(function () {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
